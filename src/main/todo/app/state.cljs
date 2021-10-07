141ad6fbcffbc975ca00e29a06e1a6f5fbb4e8cd
Ryan Kaye <ryankaye@gmail.com>
2021-10-07 17:23:30 +0700

    Remove the commented out state data

--- src/main/todo/app/state.cljs
+++ src/main/todo/app/state.cljs
@@ -11,29 +11,3 @@
                        :item "Leave the gun"
                        :completed false}}))
 
-
-;; (def inputs (r/atom {:new-input ""
-;;                      :uid 10}))
-
-;; (def data (r/atom {:inputs {:new-input ""
-;;                             :uid 10}}
-;;                   {:todos {7 {:todoid 7
-;;                               :item "Go to the mattresses"
-;;                               :completed false}
-;;                            8 {:todoid 8
-;;                               :item "Take the cannoli"
-;;                               :completed false}
-;;                            9 {:todoid 9
-;;                               :item "Leave the gun"
-;;                               :completed false}}}))
-
-
-;; (def todos (r/atom [{:todoid 7
-;;                      :item "Learn ClojureScript"
-;;                      :completed false}
-;;                     {:todoid 8
-;;                      :item "Learn Elm"
-;;                      :completed false}
-;;                     {:todoid 9
-;;                      :item "Buy milk"
-;;                      :completed false}]))
